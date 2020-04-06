2bfc3626b1fe2fe993ced8013a4fd7ad2e005bf5
Anuj <apatel922@gmail.com>
2020-04-05 20:30:42 -0400

    Added first route to index.html

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,21 @@
-// Dependencies --> fs & express
+// Dependencies --> fs, express & path
 var fs = require("fs");
 var express = require("express");
+var path = require("path");
 
 
 const app = express()
 var PORT = 8080;
 
+
+// Routes
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+  });
+
+
+
+// Listener
 app.listen(PORT, function() {
-    fs.readFile(__dirname + "/db/db.json", function(err, data) {
-        if(err) throw err;
-        notesArr = JSON.parse(data);
-    })
-    console.log("Listening on localhost:" + PORT);
-})
+    console.log("Listening on localhost: " + PORT);
+});
